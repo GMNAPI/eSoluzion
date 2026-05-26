083f638eef542b15bf7ad9420038cf8ab226288a
GMNAPI <gmnapi@240dotsdev.es>
2026-05-26 17:34:40 +0200

    test: add jest-axe a11y validation

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,8 @@
 import '@testing-library/jest-dom'
+import { toHaveNoViolations } from 'jest-axe'
 import { server } from './src/mocks/server'
+
+expect.extend(toHaveNoViolations)
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 afterEach(() => server.resetHandlers())
